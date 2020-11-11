dc196ecc69f78d6992caa7f66e873f7f4907475e
Noel Welsh <noel@untyped.com>
2020-11-11 20:17:46 +0000

    Visually distinguish closed and open tasks

--- frontend/src/main/scala/todone/component/TaskView.scala
+++ frontend/src/main/scala/todone/component/TaskView.scala
@@ -5,14 +5,21 @@ import slinky.core.annotations.react
 import slinky.web.html._
 
 import todone.data._
+import todone.data.State.Open
+import todone.data.State.Closed
 
 @react object TaskView {
   case class Props(id: Id, task: Task, close: Id => Unit)
   val component = FunctionalComponent[Props] { props =>
+    val task = props.task
+
     div(className := "flex justify-start items-center bg-white px-4 py-2")(
       div(className := "bg-white border-2 rounded border-gray-400 w-6 h-6 mr-2 hover:border-blue-500",
           onClick := (() => props.close(props.id))),
-      div(h3(props.task.title))
+      task.state match {
+	      case Open => div(h3(task.title))
+	      case Closed => div(h3(className := "line-through text-gray-600")(task.title))
+      }
     )
   }
 }

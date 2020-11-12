24648cb289a943f4ee5686de4ca0cafa8a3bd0c4
Noel Welsh <noel@untyped.com>
2020-11-12 22:10:40 +0000

    Improve styling of close button and closed state

--- frontend/src/main/scala/todone/component/Styles.scala
+++ frontend/src/main/scala/todone/component/Styles.scala
@@ -4,6 +4,7 @@ import todone.Style
 
 object Styles {
   val textDimmed = Style("text-gray-400")
+  val textHoverAccent = Style("hover:text-blue-500")
   val bgDimmed = Style("border-gray-400")
   val bgHoverAccent = Style("hover:border-blue-500")
 }

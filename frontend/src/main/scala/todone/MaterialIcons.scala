1e5a368ee25de56abf0fd232a6b5a9f7d501c4d8
Noel Welsh <noel@untyped.com>
2021-01-24 17:06:45 +0000

    Wrap some icons from "Material Icons" set

---
+++ frontend/src/main/scala/todone/MaterialIcons.scala
@@ -0,0 +1,49 @@
+package todone
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+import slinky.core.ExternalComponentNoPropsWithAttributes
+
+object MaterialIcons {
+  @JSImport("react-icons/md", "MdCheck")
+  @js.native
+  object MdCheckRaw extends js.Object
+  object MdCheck extends ExternalComponentNoPropsWithAttributes[slinky.web.html.span.tag.type] {
+    val component = MdCheckRaw
+  }
+
+  @JSImport("react-icons/md", "MdClose")
+  @js.native
+  object MdCloseRaw extends js.Object
+  object MdClose extends ExternalComponentNoPropsWithAttributes[slinky.web.html.span.tag.type] {
+    val component = MdCloseRaw
+  }
+
+  @JSImport("react-icons/md", "MdAdd")
+  @js.native
+  object MdAddRaw extends js.Object
+  object MdAdd extends ExternalComponentNoPropsWithAttributes[slinky.web.html.span.tag.type] {
+    val component = MdAddRaw
+  }
+
+  @JSImport("react-icons/md", "MdExpandMore")
+  @js.native
+  object MdExpandMoreRaw extends js.Object
+  object MdExpandMore extends ExternalComponentNoPropsWithAttributes[slinky.web.html.span.tag.type] {
+    val component = MdExpandMoreRaw
+  }
+
+  @JSImport("react-icons/md", "MdExpandLess")
+  @js.native
+  object MdExpandLessRaw extends js.Object
+  object MdExpandLess extends ExternalComponentNoPropsWithAttributes[slinky.web.html.span.tag.type] {
+    val component = MdExpandLessRaw
+  }
+
+  @JSImport("react-icons/md", "MdCheckCircle")
+  @js.native
+  object MdCheckCircleRaw extends js.Object
+  object MdCheckCircle extends ExternalComponentNoPropsWithAttributes[slinky.web.html.span.tag.type] {
+    val component = MdCheckCircleRaw
+  }
+}

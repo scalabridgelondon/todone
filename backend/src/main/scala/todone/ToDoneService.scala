94488cbff67b71125af053250861d45c92a3fe34
Noel Welsh <noel@untyped.com>
2021-02-01 21:41:23 +0000

    Add more content to the example tasks
    To make the UI more interesting to interact with, and to demonstrate
    projects and tags, add more example tasks and fill out the ones that
    were originally there.

--- backend/src/main/scala/todone/ToDoneService.scala
+++ backend/src/main/scala/todone/ToDoneService.scala
@@ -23,15 +23,41 @@ object ToDoneService {
               Id(1) -> Task(
                 State.open,
                 "Play with the ToDone interface",
-                "Click around and see what works---and what doesn't.",
-                None,
-                Tags.empty),
+                """Right now many things in the application are not working.
+                  |Our task is to make it work, and the first step to that is
+                  |finding what is needs to be fixed.""".stripMargin,
+                Some(Project("todone")),
+                Tags(List(Tag("scalabridge"), Tag("frontend")))),
+
               Id(2) -> Task(
                 State.open,
+                "Learn how to use the web developer tools",
+                """The web developer tools are one of the most useful tools for
+                  |debugging problems between the frontend and backend. We need
+                  |open up the web developer tools and look at the network pane,
+                  |where we'll find requests from the frontend that the backend
+                  |is not properly responding to.""".stripMargin,
+                Some(Project("todone")),
+                Tags(List(Tag("scalabridge"), Tag("frontend")))
+              ),
+
+              Id(3) -> Task(
+                State.open,
                 "Implement functionality to close a completed task",
-                "The close button doesn't currently work. Let's fix that as our first task.",
+                """The close button is probably the simplest bit of functionality
+                  |we can implement. (Hopefully you discovered the close button
+                  |doesn't work.) Let's do that now! The worksheets have more
+                  |details.""".stripMargin,
+                Some(Project("todone")),
+                Tags(List(Tag("scalabridge"), Tag("backend")))),
+
+              Id(4) -> Task(
+                State.open,
+                "Have a break!",
+                "We've done a lot. Time for a break.",
                 None,
-                Tags.empty)
+                Tags(List(Tag("chillout")))
+              )
             )
           )
         Ok(tasks)

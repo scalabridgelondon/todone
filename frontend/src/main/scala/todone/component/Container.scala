66433cd81008e374bbfe9083126ff6da9a4fd845
Noel Welsh <noel@noelwelsh.com>
2021-02-13 02:52:49 +0000

    Ensure white background
    This was relying on browser defaults, which is not safe.

--- frontend/src/main/scala/todone/component/Container.scala
+++ frontend/src/main/scala/todone/component/Container.scala
@@ -8,7 +8,7 @@ import slinky.core.facade.ReactElement
 @react object Container {
   case class Props(left: ReactElement, right: ReactElement)
   val component = FunctionalComponent[Props] { props =>
-    div(className := "text-gray-900")(
+    div(className := "bg-white text-gray-900")(
       Titlebar(()),
       div(className := "flex h-screen")(
         div(className := "w-1/4 px-8 py-8")(props.left),
